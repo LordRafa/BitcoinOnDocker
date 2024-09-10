47e1e4861c5d9f2aa484fa3fb15b536060e9cf87
Rafael Waldo Delgado Doblas <rawadeldo@protonmail.com>
2024-09-11 01:02:28 +0200

    fix: dockerfile user creation

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -11,11 +11,12 @@ alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
 alias ..="cd .."
+source /etc/profile.d/bash_completion.sh
 EOF
 
 fi
 
-# Default to 'DSView' if no arguments are provided
+# Default to 'bitcoind' if no arguments are provided
 args="$@"
 if [ -z "$args" ]; then
   args="bitcoind --printtoconsole"
